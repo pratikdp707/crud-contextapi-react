51b6f95f60d063a8039979002d2ecd5016c55d09
pratikdp707 <pratikparate707@gmail.com>
2021-09-24 22:00:58 +0530

    changed name navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <div className="my-5">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand p-2" to="/" style={{fontSize:"35px"}}>Navbar</Link>
+                <Link className="navbar-brand p-2" to="/" style={{fontSize:"35px"}}>Users</Link>
                 <form className="form-inline ml-auto my-2 my-lg-0 mr-3">
                     <Link to="/addUser"><button className="btn btn-primary btn-lg px-5 py-2" type="submit" style={{fontSize:"25px"}}>Add User</button></Link>
                 </form>
